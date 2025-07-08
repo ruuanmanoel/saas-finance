d45bd36e855a29244c35b527708099f9f490cc4e
Ruan Manoel <ruuanmanoel@gmail.com>
2025-07-08 19:50:42 -0300

    🔧 chore: add tailwind prettier plugin

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1>Hello World</h1>
+    <h1 className="flex h-screen items-center justify-center text-3xl font-bold underline">
+      Hello World
+    </h1>
   );
 }
